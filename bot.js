9de8d28a8725d25467604d7b40340746988617b2
Denis Chakhalian <my.chahalyan@gmail.com>
2023-12-14 15:54:53 +0200

    fix: timing

--- bot.js
+++ bot.js
@@ -89,14 +89,14 @@ bot.hears("Дата останнього посту", async (ctx) => {
       isGroupPhoto = photo.isGroup;
       photoGroupId =
         photo.media_group_id === null ? undefined : photo.media_group_id;
-      const isNightTime = currentTime.hour() >= 0 && currentTime.hour() < 19;
+      const isNightTime = currentTime.hour() >= 23 && currentTime.hour() < 20;
 
       if (
         isNightTime &&
-        currentTime?.hours() > 0 &&
-        currentTime?.hours() < 10
+        currentTime?.hours() > 23 &&
+        currentTime?.hours() < 11
       ) {
-        currentTime.set("hours", 10);
+        currentTime.set("hours", 11);
       } else {
         if (isNightTime) {
           currentTime.set("hours", currentTime.hours() + 1);
@@ -107,10 +107,10 @@ bot.hears("Дата останнього посту", async (ctx) => {
     }
   });
 
-  const isNightTime = currentTime.hour() >= 0 && currentTime.hour() < 19;
+  const isNightTime = currentTime.hour() >= 23 && currentTime.hour() < 20;
 
-  if (isNightTime && currentTime?.hours() > 0 && currentTime.hours() < 10) {
-    currentTime.set("hours", 10);
+  if (isNightTime && currentTime?.hours() > 23 && currentTime.hours() < 11) {
+    currentTime.set("hours", 11);
     currentTime.set("minutes", 0);
   } else {
     if (isNightTime) {
@@ -248,7 +248,7 @@ setInterval(() => {
 // }, 1000);
 
 function calculateSendTime(currentTime, lastPhotoSentTime) {
-  const isNightTime = currentTime.hour() >= 0 && currentTime.hour() < 19;
+  const isNightTime = currentTime.hour() >= 23 && currentTime.hour() < 20;
 
   if (lastPhotoSentTime) {
     const nextSendTime = lastPhotoSentTime
@@ -262,7 +262,7 @@ function calculateSendTime(currentTime, lastPhotoSentTime) {
 
 async function sendScheduledPhotos() {
   const currentTime = moment().tz("Europe/Kiev");
-  const isNightTime = currentTime.hour() >= 0 && currentTime.hour() < 19;
+  const isNightTime = currentTime.hour() >= 23 && currentTime.hour() < 20;
 
   if (
     count > 0 &&
@@ -283,7 +283,7 @@ async function sendScheduledPhotos() {
 
 function shouldSend(currentTime, isNightTime) {
   if (isNightTime) {
-    if (currentTime?.hours() > 0 && currentTime.hours() < 10) {
+    if (currentTime?.hours() > 23 && currentTime.hours() < 10) {
       return false;
     }
 
